0e84dd8b398925059f74c066eb47cc7afe636693
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2019-11-02 22:34:07 -0500

    loops and arrays, not resolved

--- assignments/FSW-105/Week-3/loopsNArrays/loops_and_arrays.js
+++ assignments/FSW-105/Week-3/loopsNArrays/loops_and_arrays.js
@@ -1,5 +1,52 @@
+
 var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"];   
-    for ( var i = 0; i < array.length; i++){
-        if (officeItems[i] === "computer") {
-        console.log(count)
-    }}
+var computer = "computer";
+for ( var i = 0; i < officeItems.length; i++){
+        console.log(officeItems.computer)
+        }
+
+/* other things I have tried...
+console.log("computer"++)
+console.log(officeItems.computer) with any variation of ++, "computer", officeItems."computer"
+I have tried the *count* code.
+I have tried taking out the computer variable, not sure if I did that correctly.
+*/
+
+
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+        {
+            name: "Mike",
+            age: 12, 
+            gender: "male"
+        },{
+            name: "Madeline",
+            age: 80,
+            gender: "female"
+        },{
+            name: "Cheryl",
+            age: 22,
+            gender: "female"
+        },{
+            name: "Sam",
+            age: 30,
+            gender: "male"
+        },{
+            name: "Suzy",
+            age: 4,
+            gender: "female"
+        }
+    ]
+        for ( var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
+            if (peopleWhoWantToSeeMadMaxFuryRoad.age >= 18){
+                console.log("is old enough to see Mad Max")
+            } else {
+                console.log("is not old enough to see Mad Max")
+            }
+        }
+
+// else if (peopleWho)
+
+
+
+
---
+++ assignments/FSW-110/Week3/CreatingAnLis/creating_a_lis.js
@@ -0,0 +1,13 @@
+var x = document.createElement("h1");
+
+document.body.appendChild(x);
+
+const names = ["Steve", "Larry", "Joe", "Shirley", "Steph", "Nate", "Rick", "Emily"]
+
+for (var i = 0; i < names.length; i++){
+    var y = document.createTextNode(names[i]);
+    x.appendChild(y);  
+    
+    }
+document.getElementById(names).style.color = "blue";  
+
