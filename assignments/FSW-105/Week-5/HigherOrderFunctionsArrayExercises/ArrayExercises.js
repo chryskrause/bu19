361bc25e18b261a14b1b67699234bf9b01e21bee
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2019-11-17 01:38:04 -0600

    Array Exercises, incomplete

--- assignments/FSW-105/Week-5/HigherOrderFunctionsArrayExercises/ArrayExercises.js
+++ assignments/FSW-105/Week-5/HigherOrderFunctionsArrayExercises/ArrayExercises.js
@@ -1,4 +1,4 @@
-// node ArrayExercises.js
+// filter() exercises
 // 1) Return array that only has numbers 5 and greater, using filter.
 const arr = [3, 6, 8, 2]
 const result = arr.filter(num => num >= 5)
@@ -39,6 +39,7 @@ const arr5 = [
 const result5 = arr5.filter (oldEnough => oldEnough.age > 18)
 console.log(result5)
 
+// map() exercises
 // 1) use .map() to make array of numbers that are doubles of first array
 
 const arr6 = [2, 5, 100]
@@ -57,7 +58,123 @@ console.log(result7)
 
 const arr8 = ["john", "JACOB", "jinGleHeimer", "schmidt"]
 
+const result8 = arr8.map(names => names.charAt(0).toUpperCase() + names.substr(1).toLowerCase());
+console.log(result8)
 
-// const result8 = arr8[0][1][2][3].toLowerCase().split(' ').map(word => word.replace(word[0], word[0].toUpperCase())).join(' ');
-// console.log(result8)
+// 4) Make an array of strings of the names
 
+const arr9 = [
+    {name: "Angelina Jolie", age: 80},
+    {name: "Eric Jones", age: 2},
+    {name: "Paris Hilton", age: 5},
+    {name: "Kanye West", age: 16},
+    {name: "Bob Ziroll", age: 100}
+]
+
+const result9 = arr9.map(arrOfStrings => arrOfStrings.name)
+console.log(result9)
+
+// 5) Make an array of strings of the names saying if they can go to the matrix
+
+const arr10 = [
+    {name: "Angelina Jolie", age: 80},
+    {name: "Eric Jones", age: 2},
+    {name: "Paris Hilton", age: 5},
+    {name: "Kanye West", age: 16},
+    {name: "Bob Ziroll", age: 100}
+]
+
+const result10 = arr10.map(names10 => names10.age < 18 ? console.log(names10.name + " is underage.  Cannot see movie.  It's an agent") : 
+console.log(names10.name + " is old enough to see The Matrix. Unplug them"))
+
+// 6) Make an array of the names in h1s, ages in h2s
+
+const arr11 = [
+    {name: "Angelina Jolie", age: 80},
+    {name: "Eric Jones", age: 2},
+    {name: "Paris Hilton", age: 5},
+    {name: "Kanye West", age: 16},
+    {name: "Bob Ziroll", age: 100}
+]
+
+const result11 = arr11.map(headsUp => "<h1>"+headsUp.name+"</h1>"+"<h2>"+headsUp.age+"</h2>")
+console.log(result11)
+
+
+// reduce() exercises
+// 1) Turn an array of numbers into a total of all numbers
+
+const arr12 = [1, 2, 3]
+
+const result12 = arr12.reduce(function(final, num3){
+    final += num3
+    return final
+})
+console.log(result12)
+
+// 2) Turn array of numbers into long string of numbers [1,2,3] to "123"
+
+const arr13 = [1, 2, 3]
+
+const result13 = arr13.reduce(function(final2, num4){
+    final2 = final2 + num4
+    return final2
+}, "")
+console.log(result13)
+
+// 3) Turn array of voter objects into count of how many people voted
+
+const arr14 = [
+    {name:'Bob', age: 30, voted: true},
+    {name:'Jake', age: 32, voted: true},
+    {name:'Kate', age: 25, voted: false},
+    {name:'Sam', age: 20, voted: false},
+    {name:'Phil', age: 21, voted: true},
+    {name:'Ed', age: 55, voted: true},
+    {name:'Tami', age: 54, voted: true},
+    {name:'Mary', age: 31, voted: false},
+    {name:'Becky', age: 43, voted: false},
+    {name:'Joey', age: 41, voted: true},
+    {name:'Jeff', age: 30, voted: true},
+    {name:'Zack', age: 19, voted: false}
+]
+
+const result14 = arr14.reduce(function(final3, voter){
+    if(voter.voted){
+        final3++
+    }
+    return final3
+}, 0)
+console.log(result14)
+
+// 4) Given an array, figure total costs
+
+const arr15 = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000}
+]
+
+const result15 = arr15.reduce(function(final4, pricing){
+    final4 = final4 + pricing.price
+    return final4
+}, 0)
+console.log(result15)
+
+// 5) Given array of arrays, flatten into a single array
+
+const arr16 = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+const result16 = arr16.reduce(function(final5, arrOfArrs){
+    final5 = final5.concat(arrOfArrs)
+    return final5
+})
+console.log(result16)
+
+// node ArrayExercises.js
