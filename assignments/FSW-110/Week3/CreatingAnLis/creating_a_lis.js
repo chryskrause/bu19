4dd03c9825168e95147f8f3db054956a65614915
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2019-11-19 23:13:37 -0600

    Creating a lis assignment completed.

--- assignments/FSW-110/Week3/CreatingAnLis/creating_a_lis.js
+++ assignments/FSW-110/Week3/CreatingAnLis/creating_a_lis.js
@@ -1,26 +1,18 @@
-var x = document.createElement("HEADER");
-x.setAttribute("id", "myHeader");
-document.body.appendChild(x);
+const names = ["Steve", "Larry", "Joe", "Shirley", "Steph", "Nate", "Rick", "Emily"]
 
-var y = document.createElement("h1");
-var t = document.createTextNode("Hello World");
-y.appendChild(t);
+for (let i = 0; i < names.length; i++) {
+    var x = document.createElement("HEADER");
+    x.setAttribute("id", "myHeader");
+    document.body.appendChild(x);
 
-for (let i = 0; i < 10; i++) {
-    
+    var y = document.createElement("h1");
+    var t = document.createTextNode(names[i]);
+    y.appendChild(t);
+
+    document.getElementById("myHeader").appendChild(y).style.color = "red";
 }
 
-document.getElementById("myHeader").appendChild(y);
-// const names = ["Steve", "Larry", "Joe", "Shirley", "Steph", "Nate", "Rick", "Emily"]
+document.getElementById("myHeader").style.textDecoration = "underline";
 
-// for (var i = 0; i < names.length; i++){
-//     var y = document.createTextNode(names[i]);
-//     x.appendChild(y);
-    
-//     }
-
-//
-//document.getElementsByTagName(names).style.color = "red";
-
-
+document.body.style.backgroundColor = "lightgray";
 
--- assignments/FSW-110/Week4/HTML and JS calculator/calculatorjs.js
+++ assignments/FSW-110/Week4/HTML and JS calculator/calculatorjs.js
@@ -9,16 +9,21 @@ document.body.style.backgroundColor = "lightblue";
     */
 
 //const addAll = document.getElementById("add");
-const number1 = document.getElementById("num1");
-const number2 = document.getElementById("num2");
-document.getElementById("addbutton").addEventListener("onclick", answer1());
-function answer1 (number1, number2){
-     (number1 + number2);    
-}
-answer1(number1, number2)
+const number1 = document.getElementById("num1").value;
+const number2 = document.getElementById("num2").value;
+const adds = document.getElementById("add");
+adds.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const answer1 = function sum (){
+        return (number1 + number2).innerHTML
+
+    }
+})
+sum();
+// document.getElementById("addbutton").addEventListener("submit", answer1());
+// function answer1 (number1, number2){
+//      (number1 + number2);    
+// }
+// answer1(number1, number2)
 
 
-/*Note: even when I tried an alert, I got NaN and that number1 and number2 are undefined or something.
-Tried some things with value
-Tried working in some innerHTML after the eventListener ()
-*/
