d4e4af335a3375260fdad898db198bc6b4000e0d
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2019-11-20 22:42:22 -0600

    FSW110 calculator assignment. complete

--- assignments/FSW-110/Week4/HTML and JS calculator/calculatorjs.js
+++ assignments/FSW-110/Week4/HTML and JS calculator/calculatorjs.js
@@ -1,29 +1,42 @@
 document.body.style.backgroundColor = "lightblue";
 
-/*
-<div id="add">
-        Addition <input type="number" id="num1"/>
-        <input type="number" id="num2"/>
-        <button type="button" id="addbutton">Get Answer</button>
-    </div>
-    */
+addNumbers = (number1, number2) => {
+    return number1 + number2
+}
 
-//const addAll = document.getElementById("add");
-const number1 = document.getElementById("num1").value;
-const number2 = document.getElementById("num2").value;
-const adds = document.getElementById("add");
-adds.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const answer1 = function sum (){
-        return (number1 + number2).innerHTML
+document.getElementById('addBtn').addEventListener("click", () => {
+    const num1 = parseInt(document.getElementById('numAdd1').value)
+    const num2 = parseInt(document.getElementById('numAdd2').value)
+   
+    const additionResult = addNumbers(num1, num2)
 
-    }
+   
+   document.getElementById('showAddition').innerHTML = additionResult
+});
+
+subtractNumbers = (number3, number4) => {
+    return number3 - number4
+}
+
+document.getElementById("subtractBtn").addEventListener("click", () => {
+    const num3 = parseInt(document.getElementById("numSub3").value)
+    const num4 = parseInt(document.getElementById("numSub4").value)
+
+    const subtractResult = subtractNumbers(num3, num4)
+
+    document.getElementById("showSubtraction").innerHTML = subtractResult
+});
+
+multiplyNumbers = (number5, number6) => {
+    return number5 * number6
+}
+
+document.getElementById("multBtn").addEventListener("click", () => {
+    const num5 = parseInt(document.getElementById("num5").value)
+    const num6 = parseInt(document.getElementById("num6").value)
+
+    const multiplyResult = multiplyNumbers(num5, num6)
+
+    document.getElementById("showMultiply").innerHTML = multiplyResult
 })
-sum();
-// document.getElementById("addbutton").addEventListener("submit", answer1());
-// function answer1 (number1, number2){
-//      (number1 + number2);    
-// }
-// answer1(number1, number2)
-
 
