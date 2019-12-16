26460c57a84b65f8b72917df53f409ea9a409539
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2019-12-15 22:50:53 -0600

    To Do List

---
+++ assignments/FSW-120/Week1/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function App(){
+  return(
+    <div>
+      <input type="checkbox"/> Learn React
+      <p>Shouldn't take more than an hour</p>
+      <input type="checkbox"/>Take over the world
+      <p>Also, shouldn't take more than an hour</p>
+      <input type="checkbox"/>Kidnap baby Yoda
+      <p>This might take two hours</p>
+    </div>
+  )
+}
+
+export default App;
+
