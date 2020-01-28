5789a2cbe7c4690075b2e85ce0f1621475cfae11
Chrys Krause <chrystal.krause@bryanuniversity.edu>
2020-01-27 19:30:47 -0600

    dice roll

--- assignments/FSW-120/Week3/DiceBox/my-app/src/App.js
+++ assignments/FSW-120/Week3/DiceBox/my-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import DiceBox from "./DiceBox";
 import './App.css';
 
 function App() {
--- assignments/FSW-120/Week3/DiceBox/my-app/src/DiceBox.js
+++ assignments/FSW-120/Week3/DiceBox/my-app/src/DiceBox.js
@@ -10,17 +10,39 @@ class DiceBox extends React.Component {
             num4: 0,
             num5: 0
         }
+        this.handleClick = this.handleClick.bind(this)
     }
-diceRoll = Math.floor(Math.random() * 6)
+// diceRoll = Math.floor(Math.random() * 6)
+handleClick(){
+    this.setState(prevState => {
+        return{
+            num1: prevState.num1 + (Math.floor(Math.random() * 6)),
+            num2: prevState.num2 + (Math.floor(Math.random() * 6)),
+            num3: prevState.num3 + (Math.floor(Math.random() * 6)),
+            num4: prevState.num4 + (Math.floor(Math.random() * 6)),
+            num5: prevState.num5 + (Math.floor(Math.random() * 6))
+        }
+    })
+}
 
 render(){
     return(
         <div>
-            {this.diceroll}
-            <button>Roll!</button>
+            {this.state.num1}<br></br>
+            {this.state.num2}<br></br>
+            {this.state.num3}<br></br>
+            {this.state.num4}<br></br>
+            {this.state.num5}<br></br>
+            <button onClick={this.handleClick}>Roll!</button>
         </div>
     )
 }
 }
 
 export default DiceBox
+
+/*  Other things I tried
+num1: prevState.num1.diceRoll - said diceRoll undefined
+num1: prevState.num1(diceRoll)
+num1: prevState.num1=diceRoll
+*/
